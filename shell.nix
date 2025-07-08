7fd64033102425fdd44522a5384a4a1d7a6ec6b4
midischwarz12 <midischwarz@proton.me>
2025-07-08 18:33:30 -0500

    feat: added flake

--- shell.nix
+++ shell.nix
@@ -1,10 +1,15 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import <nixpkgs> { }, fetchFromGitHub }:
 with pkgs.python3Packages;
 
 let manim_utils = buildPythonPackage rec {
   pname = "manim_utils";
   version = "0.1.0";
-  src = ../manim_utils;
+  src = fetchFromGitHub {
+    owner = "Getting-to-One";
+    repo = "manim_utils";
+    rev = "main";
+    hash = "sha256-g56FMBQ7ni+lxqfWiF4dyJilZuRuKL5XXZcHcDUSPME=";
+  };
   doCheck = false; # check requires pip
   format = "pyproject";
   buildInputs = [
