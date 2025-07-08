c52fa27d24c29ce1c5d94b38896b816472fcca8f
midischwarz12 <midischwarz@proton.me>
2025-07-08 18:44:06 -0500

    refactor: nixfmt

--- flake.nix
+++ flake.nix
@@ -2,15 +2,15 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
   outputs =
-  inputs@{
-    self,
-    nixpkgs,
-    ...
-  }:
+    inputs@{
+      self,
+      nixpkgs,
+      ...
+    }:
     let
       pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      devShells.x86_64-linux.default = pkgs.callPackage (self + "/shell.nix") {};
+      devShells.x86_64-linux.default = pkgs.callPackage (self + "/shell.nix") { };
     };
 }
