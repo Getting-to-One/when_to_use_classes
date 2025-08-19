f59774d5177012926202b2a892e71c8cbd60605d
emilyrhee <emilyrhee99@gmail.com>
2025-08-19 12:11:19 -0400

    using fork of manim for `code_lines` height bug fix
    can go back to previous flake and shell once new version of manim is released

--- flake.nix
+++ flake.nix
@@ -1,5 +1,10 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs = {
+    type = "github";
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "21808d22b1cda1898b71cf1a1beb524a97add2c4"; 
+  };
 
   outputs =
     inputs@{
--- shell.nix
+++ shell.nix
@@ -2,9 +2,58 @@
   pkgs ? import <nixpkgs> { },
   fetchFromGitHub,
 }:
+
 with pkgs.python3Packages;
 
 let
+  manim_fork = buildPythonPackage rec {
+    pname = "manim";
+    version = "0.19.0";
+    src = fetchFromGitHub {
+      owner = "behackl";
+      repo = "manim";
+      rev = "codemobject-fix-paragraph-lines";
+      hash = "sha256-rxHB95EJP8n6nU7KegehNp2UTpbBVz/85b1fd1T9YJ4=";
+    };
+
+    format = "pyproject";
+    build-system = [
+      hatchling
+    ];
+
+    buildInputs = [
+      hatchling
+    ];
+
+    propagatedBuildInputs = with pkgs; [
+      pygments
+      scipy
+      pydub
+      rich
+      numpy
+      pycairo
+      ipython
+      pillow
+      click
+      av
+      beautifulsoup4
+      cloup
+      isosurfaces
+      manimpango
+      mapbox-earcut
+      moderngl
+      moderngl-window
+      networkx
+      screeninfo
+      skia-pathops
+      python312Packages.srt
+      svgelements
+      tqdm
+      typing-extensions
+      python312Packages.watchdog
+    ];
+  };
+
   manim_utils = buildPythonPackage rec {
     pname = "manim_utils";
     version = "0.1.0";
@@ -14,20 +63,20 @@ let
       rev = "main";
       hash = "sha256-OsIR+u+x8bVk86Ocugzl4waEnlV/k2KBuevcOAO0Yl8=";
     };
-    doCheck = false; # check requires pip
+    doCheck = false;
     format = "pyproject";
     buildInputs = [
       setuptools
     ];
     propagatedBuildInputs = [
-      manim
+      manim_fork
     ];
   };
 in
 pkgs.mkShell {
   packages = with pkgs; [
     python3
-    manim
+    manim_fork
     manim_utils
   ];
 }
